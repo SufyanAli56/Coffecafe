54a18cd91eba94fb4c9a8233f7e299c87a699278
SufyanAli56 <sufyanalii1127@gmail.com>
2024-12-26 02:26:07 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Services from './components/Services/Services';
+import Banner from './components/Banner/Banner';
+import AppStore from './components/AppStore/AppStore';
+import Testmoinal from './components/Testmoinal/Testmoinal';
+import Footer from './components/Footer/Footer';
+
+const App = () => {
+  useEffect(()=>{
+        AOS.init(
+          {
+            offset:100,
+            duration:700,
+            easing:"ease-in",
+            delay:100,
+          }
+        )
+  })
+  return (
+    <div className=" overflow-x-hidden">
+      <Navbar/>
+      <Home />
+      <Services/>
+      <Banner/>
+  <AppStore/>
+  <Testmoinal/>
+  <Footer/>
+    </div>
+  );
+}
+
+export default App;
